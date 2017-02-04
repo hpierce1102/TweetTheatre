2b75476a3502df560670c7898a94bad8f218741d
Hayden Pierce <hayden@haydenpierce.com>
2017-02-04 03:38:36 -0600

    Added actual twitter dummy image.

--- src/entity/ExampleTwitterUsers.ts
+++ src/entity/ExampleTwitterUsers.ts
@@ -3,13 +3,13 @@ import {TwitterUser} from  "./TwitterUser";
 const user1 = new TwitterUser();
 user1.username = "@hpierce1102";
 user1.realName = "Hayden Pierce";
-user1.profileImageUrl = "example.com/image.jpg";
+user1.profileImageUrl = "https://abs.twimg.com/a/1486092131/img/t1/highline/empty_state/owner_empty_avatar.png";
 user1.verified = false;
 
 const user2 = new TwitterUser();
 user2.username = "@example123";
 user2.realName = "Jane Doe";
-user2.profileImageUrl = "example.com/image.png";
+user2.profileImageUrl = "https://abs.twimg.com/a/1486092131/img/t1/highline/empty_state/owner_empty_avatar.png";
 user2.verified = false;
 
 const user3 = new TwitterUser();
